7a6b44f67c7c9dbdd4aef4f591f1533c24e8dd59
Dominik Winecki <dominikwinecki@gmail.com>
2018-03-02 19:30:28 -0500

    Add auto grab in teleop

--- src/org/usfirst/frc/team1014/robot/commands/teleop/TeleGrab.java
+++ src/org/usfirst/frc/team1014/robot/commands/teleop/TeleGrab.java
@@ -19,6 +19,10 @@ public class TeleGrab extends Command {
 	boolean heartbeatState = false, heartbeatDown = false;
 	long startLastGrab = 0;
 
+	boolean autoGrab = false;
+	private static final int AUTO_GRAB_LIFT_TIME = 450;
+	long autoGrabLiftUntil = 0;
+
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
@@ -27,51 +31,70 @@ public class TeleGrab extends Command {
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
-		if (controller.getTriggerAxis(Hand.kRight) > .3) {
-			// Collect cubes
+		if (controller.getYButton()) {
+			// Autograb
 			grabber.turnCollect(1);
-			heartbeatState = false;
-		} else if (controller.getBumper(Hand.kRight)) {
-			// release
-			grabber.turnRelease(.6);
-			heartbeatState = false;
+			lifter.move(-.8, false);
+			autoGrab = true;
 		} else {
-			if (heartbeatState) {
-				grabber.turnCollect(isGrabbing() ? .2 : 0);
-				controller.setRumble(RumbleType.kLeftRumble, isGrabbing() ? 1 : 0);
-				controller.setRumble(RumbleType.kRightRumble, isGrabbing() ? 1 : 0);
+			boolean forceLift = false;
+			if (autoGrab) {
+				autoGrab = false;
+				// Auto grab ended
+				autoGrabLiftUntil = System.currentTimeMillis() + AUTO_GRAB_LIFT_TIME;
+				heartbeatState = true;
+			}
+			forceLift = System.currentTimeMillis() - autoGrabLiftUntil < 0;
 
+			if (controller.getTriggerAxis(Hand.kRight) > .3) {
+				// Collect cubes
+				grabber.turnCollect(1);
+				heartbeatState = false;
+			} else if (controller.getBumper(Hand.kRight)) {
+				// release
+				grabber.turnRelease(.6);
+				heartbeatState = false;
 			} else {
-				grabber.turnCollect(0);
-				controller.setRumble(RumbleType.kLeftRumble, 0);
-				controller.setRumble(RumbleType.kRightRumble, 0);
-			}
-		}
+				if (heartbeatState) {
+					grabber.turnCollect(isGrabbing() ? .2 : 0);
+					controller.setRumble(RumbleType.kLeftRumble, isGrabbing() ? 1 : 0);
+					controller.setRumble(RumbleType.kRightRumble, isGrabbing() ? 1 : 0);
 
-		if (controller.getAButton()) {
-			if (!heartbeatDown) {
-				heartbeatDown = true;
-				heartbeatState = !heartbeatState;
-				startLastGrab = System.currentTimeMillis();
+				} else {
+					grabber.turnCollect(0);
+					controller.setRumble(RumbleType.kLeftRumble, 0);
+					controller.setRumble(RumbleType.kRightRumble, 0);
+				}
 			}
-		} else {
-			heartbeatDown = false;
-		}
 
-		BadLog.publish("Grabber/Heartbeat", LogUtil.fromBool(heartbeatState));
-
-		{
-			double speed = (controller.getBumper(Hand.kLeft) ? 1 : 0)
-					- (controller.getTriggerAxis(Hand.kLeft) > .5 ? 1 : 0);
-			boolean overrideLimits = false;
-			
-			if(Math.abs(controller.getY(Hand.kRight)) > .05) {
-				speed = -controller.getY(Hand.kRight);
-				overrideLimits = true;
+			if (controller.getAButton()) {
+				if (!heartbeatDown) {
+					heartbeatDown = true;
+					heartbeatState = !heartbeatState;
+					startLastGrab = System.currentTimeMillis();
+				}
+			} else {
+				heartbeatDown = false;
 			}
-			
-			lifter.move(speed, overrideLimits);
+
+			BadLog.publish("Grabber/Heartbeat", LogUtil.fromBool(heartbeatState));
+
+			{
+				double speed = (controller.getBumper(Hand.kLeft) ? 1 : 0)
+						- (controller.getTriggerAxis(Hand.kLeft) > .5 ? 1 : 0);
+				boolean overrideLimits = false;
+
+				if (forceLift) {
+					speed = 1;
+				}
+
+				if (Math.abs(controller.getY(Hand.kRight)) > .05) {
+					speed = -controller.getY(Hand.kRight);
+					overrideLimits = true;
+				}
+
+				lifter.move(speed, overrideLimits);
+			}
 		}
 	}
 
